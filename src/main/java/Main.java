dde6acde6d1b77b0ac4059fd7b772a0d79c6eec6
Kushtanov Ivan <90986696+Ibaaan@users.noreply.github.com>
2026-03-03 02:33:39 +0500

    №1 вариант 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("№1 Введите n [0, ∞)");
+        int n = sc.nextInt();
+        RandomArray.avgElement(n);
+        System.out.println();
+
+
+    }
 }
