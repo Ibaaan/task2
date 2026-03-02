9ce8d9b70f7e7803a00681a3ed1c5accf804a9ea
Kushtanov Ivan <90986696+Ibaaan@users.noreply.github.com>
2026-03-03 02:47:05 +0500

    №2 вариант 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,12 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int n;
 
         System.out.println("№1 Введите n [0, ∞)");
-        int n = sc.nextInt();
+        n = sc.nextInt();
         RandomArray.avgElement(n);
         System.out.println();
 
-
+        System.out.println("№2 Введите n [0, ∞)");
+        n = sc.nextInt();
+        ListWork.bubbleSort(n);
+        System.out.println();
     }
 }
